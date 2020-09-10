bda7d189a72a6c76d9b6897bd690b8c1776da7de
jangseoyoung <jangseoyoung0402@gmail.com>
2020-09-10 23:18:03 +0900

    첫 ios 커밋 & 미완성

---
+++ ViewTransition/ViewTransition/UserInformation.swift
@@ -0,0 +1,17 @@
+//
+//  UserInformation.swift
+//  ViewTransition
+//
+//  Created by 장서영 on 2020/09/10.
+//  Copyright © 2020 장서영. All rights reserved.
+//
+
+import Foundation
+
+class UserInformation{
+    static let shared : UserInformation = UserInformation()
+    
+    var name: String?
+    var age: String?
+    
+}
---
+++ ViewTransition/ViewTransition/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  ViewTransition
+//
+//  Created by 장서영 on 2020/09/10.
+//  Copyright © 2020 장서영. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameField : UITextField!
+    @IBOutlet weak var ageField : UITextField!
+    
+    @IBAction func touchUpSetButton(_ sender : UIButton){
+        UserInformation.shared.name = nameField.text
+        UserInformation.shared.age = ageField.text
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("Viewcontroller의 view가 메모리에 로드됨")
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("ViewController의 view가 화면에 보여질 예정")
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("ViewController의 view가 화면에 보여짐")
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        print("ViewController의 view가 화면에서 사라질 예정")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        print("ViewController의 view가 화면에서 사라짐")
+    }
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        print("ViewController의 view가 subview를 레이아웃 하려함")
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        print("ViewController의 view가 subview를 레이아웃 함")
+    }
+
+
+}
+
