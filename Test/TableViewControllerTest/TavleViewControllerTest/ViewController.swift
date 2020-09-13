7a8568a4723623951d0dc4c63ad72939b317a283
jangseoyoung <jangseoyoung0402@gmail.com>
2020-09-14 08:34:14 +0900

    정리

---
+++ Test/TableViewControllerTest/TavleViewControllerTest/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TavleViewControllerTest
+//
+//  Created by 장서영 on 2020/09/14.
+//  Copyright © 2020 장서영. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
