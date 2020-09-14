7b8f1a30946c4431cba6d04bc6a3fe511acae8ac
jangseoyoung <jangseoyoung0402@gmail.com>
2020-09-14 20:12:56 +0900

    Create & Update : TableView(Controller)

---
+++ TableView(Controller)연습/TestTableViewController_2/TestTableViewController_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTableViewController_2
+//
+//  Created by 장서영 on 2020/09/14.
+//  Copyright © 2020 장서영. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
