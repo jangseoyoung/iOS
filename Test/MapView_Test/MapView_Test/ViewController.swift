ec98ea086dbbb836616779fb2d50d5a725a811b4
jangseoyoung <jangseoyoung0402@gmail.com>
2021-02-06 23:43:08 +0900

    Update : MapView_Test

--- Test/MapView_Test/MapView_Test/ViewController.swift
+++ Test/MapView_Test/MapView_Test/ViewController.swift
@@ -22,8 +22,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         lbLocationInfo.text = ""
         lbLocationInfo2.text = ""
         locationManager.delegate = self
+        // 정확도 최고로 설정
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        // 위치 데이터 추적을 위해 사용자에게 승인 요구
         locationManager.requestWhenInUseAuthorization()
+        // 위치 업데이트 시작
         locationManager.startUpdatingLocation()
         myMap.showsUserLocation = true
     }
@@ -36,6 +39,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         return pLocation
     }
     
+    // 특정 위/경도에 핀 설치, 핀에 타이틀&서브타이틀 문자열 표시
     func setAnnotation(latitudeValue: CLLocationDegrees, longtitudeValue: CLLocationDegrees, delta span: Double, title strTitle: String, subtitle strsubtitle: String){
         let annotation = MKPointAnnotation()
         annotation.coordinate = goLocation(latitudeValue: latitudeValue, longitudeValue: longtitudeValue, delta: span)
@@ -44,6 +48,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         myMap.addAnnotation(annotation)
     }
     
+    // 위치 정보에서 국가, 지역, 도로 추출해 레이블에 표시
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let pLocation = locations.last
         _=goLocation(latitudeValue: (pLocation?.coordinate.latitude)!, longitudeValue: (pLocation?.coordinate.longitude)!, delta: 0.01)
@@ -67,18 +72,22 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.stopUpdatingLocation()
     }
     
+    // 세그먼트 컨트롤을 선택하였을 때 호출
     @IBAction func sgChangeLocation(_ sender: UISegmentedControl){
         if sender.selectedSegmentIndex == 0 {
+            // 현재 위치 선택
             self.lbLocationInfo.text = ""
             self.lbLocationInfo2.text = ""
             locationManager.startUpdatingLocation()
         }
         else if sender.selectedSegmentIndex == 1{
+            // 폴리텍대학 선택
             setAnnotation(latitudeValue: 37.751853, longtitudeValue: 128.87605740000004, delta: 1, title: "한국폴리텍대학 강릉캠퍼스", subtitle: "강원도 강릉시 남산초교길 121")
             self.lbLocationInfo.text = "보고 계신 위치"
             self.lbLocationInfo2.text = "한국폴리텍대학 강릉캠퍼스"
         }
         else if sender.selectedSegmentIndex == 2 {
+            // 이지스퍼블리싱 선택
             setAnnotation(latitudeValue: 37.556876, longtitudeValue: 126.914066, delta: 0.1, title: "이지스퍼블리싱", subtitle: "서울시 마포구 잔다리로 109 이지스 빌딩")
             self.lbLocationInfo.text = "보고 계신 위치"
             self.lbLocationInfo2.text = "이지스퍼블리싱 출판사"
