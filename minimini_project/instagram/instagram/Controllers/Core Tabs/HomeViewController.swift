d5156003947a9d2a16dd51c8d73d4430ef71b58a
jangseoyoung <jangseoyoung0402@gmail.com>
2021-02-07 23:58:04 +0900

    Create : instagram

---
+++ minimini_project/instagram/instagram/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagram
+//
+//  Created by 장서영 on 2021/02/07.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
