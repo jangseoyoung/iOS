b8f047484a0b5ab9219ed8b7cc49a15c9ccdc742
jangseoyoung <jangseoyoung0402@gmail.com>
2021-02-18 23:52:46 +0900

    Update : iOS

--- minimini_project/instagram/instagram/Resources/AppDelegate.swift
+++ minimini_project/instagram/instagram/Resources/AppDelegate.swift
@@ -5,7 +5,9 @@
 //  Created by 장서영 on 2021/02/07.
 //
 
+
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp
         return true
     }
 
