778617356d229cf70e30dcab3a1c3cd9236c4496
jangseoyoung <jangseoyoung0402@gmail.com>
2021-03-05 22:40:43 +0900

    Update : Sketch_Test

--- Test/Sketch_Test/Sketch_Test/ViewController.swift
+++ Test/Sketch_Test/Sketch_Test/ViewController.swift
@@ -9,26 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var backgroundView: UIView!
+    
     // 색 변경 버튼 아웃렛
     @IBOutlet weak var redButton : UIButton!
     @IBOutlet weak var greenButton: UIButton!
-    @IBOutlet weak var blackButton: UIButton!
+    @IBOutlet weak var yellowButton: UIButton!
+    
+    @IBOutlet weak var lineStepper: UIStepper!
     
     // 스케치북 (이미지뷰) 아웃렛
     @IBOutlet var imgView: UIImageView!
     
+    @IBOutlet var clearButton : UIButton!
+    
     var lastPoint: CGPoint! // 바로 전에 터치 or 이동한 위치
-    var lineSize: CGFloat = 2.0 // 선 두께를 2.0으로 설정
-    var lineColor = UIColor.blue.cgColor // 선 색상을 파란색으로 설정(기본값)
+    var lineSize: CGFloat = 1.0 // 선 두께를 2.0으로 설정
+    var lineColor = UIColor.black // 선 색상을 파란색으로 설정(기본값)
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.redButton.layer.cornerRadius = 15
-        self.greenButton.layer.cornerRadius = 15
-        self.blackButton.layer.cornerRadius = 15
+       
         self.imgView.layer.borderColor = UIColor.black.cgColor
-        self.imgView.layer.borderWidth = 3
+        self.imgView.layer.borderWidth = 1
+            
+            
+        
+        buttonStyle(button: redButton)
+        buttonStyle(button: greenButton)
+        buttonStyle(button: yellowButton)
+        
+        self.lineStepper.minimumValue = 0.5
+        self.lineStepper.maximumValue = 30.0
+        self.lineStepper.stepValue = 1.0
+        
+        self.clearButton.layer.cornerRadius = 10
+        
+        self.lineStepper.layer.cornerRadius = 7
+    
     }
     
     // 모두 지우기
@@ -38,30 +58,37 @@ class ViewController: UIViewController {
     
     // 빨간색 버튼이 눌렸을 때
     @IBAction func redButton(_ sender: UIButton){
-        if lineColor == UIColor.red.cgColor { //현재 펜 색이 빨간색일 때 누르면
-            lineColor = UIColor.blue.cgColor // 펜 색을 다시 파랑으로 바꿈.
+        if lineColor == UIColor.red { //현재 펜 색이 빨간색일 때 누르면
+            lineColor = UIColor.black // 펜 색을 다시 파랑으로 바꿈.
         } else { // 현재 펜 색이 다른 색일 때
-            lineColor = UIColor.red.cgColor // 펜 색을 빨간색으로 바꿈.
+            lineColor = UIColor.red // 펜 색을 빨간색으로 바꿈.
+//            backgroundView.backgroundColor = UIColor.red
         }
     }
     
     // 초록색 버튼이 눌렸을 때
     @IBAction func greenButton(_ sender: UIButton){
-        if lineColor == UIColor.green.cgColor { // 현재 펜 색이 초록색일 때 누르면
-            lineColor = UIColor.blue.cgColor // 펜 색을 다시 파랑으로 바꿈.
+        if lineColor == UIColor.green { // 현재 펜 색이 초록색일 때 누르면
+            lineColor = UIColor.black // 펜 색을 다시 파랑으로 바꿈.
             
         } else { // 현재 펜 색이 다른 색일 때
-            lineColor = UIColor.green.cgColor // 펜 색을 초록색으로 바꿈.
+            lineColor = UIColor.green // 펜 색을 초록색으로 바꿈.
+//            backgroundView.backgroundColor = UIColor.green
         }
     }
     
     // 검은색 버튼이 눌렸을 때
-    @IBAction func blackButton(_ sender: UIButton){
-        if lineColor == UIColor.black.cgColor { //현재 펜 색이 검정일 때
-            lineColor = UIColor.blue.cgColor // 펜 색을 파란색으로 바꿈.
+    @IBAction func yellowButton(_ sender: UIButton){
+        if lineColor == UIColor.yellow { //현재 펜 색이 검정일 때
+            lineColor = UIColor.black // 펜 색을 파란색으로 바꿈.
         } else { // 현재 펜 색이 다를 때
-            lineColor = UIColor.black.cgColor // 펜 색을 검은색으로 바꿈
+            lineColor = UIColor.yellow // 펜 색을 검은색으로 바꿈
+//            backgroundView.backgroundColor = UIColor.yellow
         }
+    }
+    
+    @IBAction func lineStepper(_ sender: UIStepper){
+        lineSize = CGFloat(lineStepper.value)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -70,9 +97,10 @@ class ViewController: UIViewController {
         lastPoint = touch.location(in: imgView) // 터치된 위치를 lastPoint에 할당
     }
     
+   
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         UIGraphicsBeginImageContext(imgView.frame.size) // 그림을 그리기 위한 콘텍스트 생성
-        UIGraphicsGetCurrentContext()?.setStrokeColor(lineColor) // 선 색상 설정
+        UIGraphicsGetCurrentContext()?.setStrokeColor(lineColor.cgColor) // 선 색상 설정
         UIGraphicsGetCurrentContext()?.setLineCap(CGLineCap.round) // 선 끝 모양 라운드로 설정
         UIGraphicsGetCurrentContext()?.setLineWidth(lineSize) // 선 두께 설정
         
@@ -94,7 +122,7 @@ class ViewController: UIViewController {
     // 위와 동일
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         UIGraphicsBeginImageContext(imgView.frame.size)
-        UIGraphicsGetCurrentContext()?.setStrokeColor(lineColor)
+        UIGraphicsGetCurrentContext()?.setStrokeColor(lineColor.cgColor)
         UIGraphicsGetCurrentContext()?.setLineCap(CGLineCap.round)
         UIGraphicsGetCurrentContext()?.setLineWidth(lineSize)
         
@@ -114,6 +142,11 @@ class ViewController: UIViewController {
         }
     }
     
+    func buttonStyle(button: UIButton){
+        button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.cornerRadius = 16
+    }
     
 }
 
